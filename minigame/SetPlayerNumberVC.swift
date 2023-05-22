505cd8c6ab2e2d306f6f8974affdb7aecec53a9b
DEMl4N <delfirmamentos@gmail.com>
2023-05-22 18:03:36 +0900

    Feat: 쉐킷쉐킷 게임 구현

--- minigame/SetPlayerNumberVC.swift
+++ minigame/SetPlayerNumberVC.swift
@@ -21,7 +21,14 @@ class SetPlayerNumberVC: UIViewController {
     
     @IBAction func startClick(_ sender: Any) {
         guard let playerNumber = Int(playerNumberTextField.text ?? "1") else { return }
-    
+        
+        let gameSB = UIStoryboard(name: gameID, bundle: Bundle.main)
+        if (gameID == "ShakeIt") {
+            guard let gameVC = gameSB.instantiateViewController(withIdentifier: gameID) as? ShakeItGameVC else { return }
+            
+            gameVC.playerNumber = playerNumber
+            self.present(gameVC, animated: true)
+        }
     }
 
 }
